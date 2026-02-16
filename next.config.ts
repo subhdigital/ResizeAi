d02c44d811bc11f2715d4d805d65afcc5497d1f8
VishalThakur25 <vishalsingh25269@gmail.com>
2026-02-17 05:14:22 +0530

    pwa fix

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,12 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
   workboxOptions: {
     disableDevLogs: true,
+    skipWaiting: true,
   },
 });
 
